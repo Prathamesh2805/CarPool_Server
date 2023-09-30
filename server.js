0e37821a08c253aa7fed619f6c417ac8952729b7
Prathamesh Pande <pandepm28@gmail.com>
2023-09-30 12:04:36 +0530

    updated

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 3500
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
